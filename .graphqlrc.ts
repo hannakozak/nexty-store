5e26688b4fbe52db282fe4b314af69daf50ad76d
hanna <hannawkozak@gmail.com>
2024-07-03 00:03:30 +0100

    add grapgql configuration

---
+++ .graphqlrc.ts
@@ -0,0 +1,29 @@
+import { loadEnvConfig } from "@next/env";
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+loadEnvConfig(process.cwd());
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: process.env.GRAPHQL_URL,
+  documents: "src/graphql/*.graphql",
+  generates: {
+    "src/gql/": {
+      preset: "client",
+      presetConfig: {
+        fragmentMasking: false,
+      },
+      config: {
+        useTypeImports: true,
+        enumsAsTypes: true,
+        defaultScalarType: "unknown",
+        skipTypename: true,
+        documentMode: "string",
+      },
+      plugins: [],
+    },
+  },
+  ignoreNoDocuments: true,
+};
+
+export default config;
