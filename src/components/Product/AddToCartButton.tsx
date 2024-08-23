88748d3434d1c99b04628435a809212fda238288
hanna <hannawkozak@gmail.com>
2024-08-23 13:30:42 +0100

    style: fiz styles in addtocart button

--- src/components/Product/AddToCartButton.tsx
+++ src/components/Product/AddToCartButton.tsx
@@ -2,7 +2,7 @@ export const AddToCartButton = () => {
 	return (
 		<button
 			type="submit"
-			className="brighness-100 font-semiboldbold my-10 mb-10 h-14 w-full rounded-md bg-gradient-to-r from-gray-900 to-amber-800 text-xl text-white shadow-md hover:brightness-125 disabled:cursor-wait disabled:brightness-50"
+			className="brighness-100 font-semiboldbold my-10 mb-10 h-14 w-full rounded-md bg-amber-600 text-xl text-white shadow-md hover:bg-amber-800 disabled:cursor-wait disabled:brightness-50"
 		>
 			Add To Card
 		</button>
