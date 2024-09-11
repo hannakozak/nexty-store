5dff686f7939c20303132674096bcdbdf0b55554
hanna <hannawkozak@gmail.com>
2024-09-11 23:09:59 +0100

    fix: update product average rating

--- src/app/product/actions.ts
+++ src/app/product/actions.ts
@@ -2,7 +2,12 @@
 
 import { revalidatePath } from "next/cache";
 import { createReview, publishReview } from "@/api/reviews";
-import { type ReviewItemFragment } from "@/gql/graphql";
+import {
+	ProductGetReviewsRatingDocument,
+	ProductUpdateAverageRatingDocument,
+	type ReviewItemFragment,
+} from "@/gql/graphql";
+import { executeGraphql } from "@/api/graphqlApi";
 
 export const addReviewAction = async (productId: string, formData: FormData) => {
 	const reviewForm: ReviewItemFragment = {
@@ -21,6 +26,34 @@ export const addReviewAction = async (productId: string, formData: FormData) => 
 	}
 
 	await publishReview(reviewId.id);
+
+	let averageRating = 0;
+
+	const productReviewsRating = await executeGraphql({
+		query: ProductGetReviewsRatingDocument,
+		variables: {
+			id: productId,
+		},
+	});
+
+	if (productReviewsRating && productReviewsRating.reviewsConnection.edges.length > 0) {
+		const total = productReviewsRating.reviewsConnection.edges.reduce((acc, edge) => {
+			return acc + edge.node.rating;
+		}, 0);
+
+		averageRating =
+			total === 0 ? 0 : Math.floor(total / productReviewsRating.reviewsConnection.aggregate.count);
+	}
+
+	await executeGraphql({
+		query: ProductUpdateAverageRatingDocument,
+		variables: {
+			id: productId,
+			averageRating,
+		},
+		cache: "no-store",
+	});
+
 	revalidatePath(`/product/${productId}`);
 	revalidatePath(`/products`);
 };
