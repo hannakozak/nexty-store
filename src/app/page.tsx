55a3353fca9a6b7f0d7c85fd9f0d1dc89c1e30b6
hanna <hannawkozak@gmail.com>
2024-09-22 23:07:35 +0100

    fix: fix unworking see products button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ActiveLink } from "@/components/Navigation/ActiveLink";
 import Image from "next/image";
 
 export default async function Home() {
@@ -14,9 +15,16 @@ export default async function Home() {
 					to elevate your space and soothe your soul. Hand-poured with care, each candle offers a
 					unique sensory experience. Light up your moments with elegance.
 				</p>
-				<button className="brighness-100 font-semiboldbold mx-auto mt-8 flex w-1/2 justify-center rounded-md bg-amber-900 py-2 align-middle text-white shadow-md hover:brightness-125 disabled:cursor-wait disabled:brightness-50">
-					View Products
-				</button>
+				<ActiveLink
+					href="/products?page=1"
+					exact={true}
+					className="font-semibold text-gray-600 hover:text-gray-800"
+					activeClassName="text-gray-800 border border-2 border-b-gray-800"
+				>
+					<button className="brighness-100 font-semiboldbold mx-auto mt-8 flex w-1/2 justify-center rounded-md bg-amber-900 py-2 align-middle text-white shadow-md hover:brightness-125 disabled:cursor-wait disabled:brightness-50">
+						View Products
+					</button>
+				</ActiveLink>
 			</div>
 			<div className="w-full md:w-1/2">
 				<Image src="/images/front-image.png" alt="sense candles" width={650} height={650} />
